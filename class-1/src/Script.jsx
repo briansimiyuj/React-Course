9a7224c79bf1b42aea1a6891d4b7e489769b5750
Brian Simiyu <briansimiyuj@gmail.com>
2023-08-29 09:48:11 +0300

    Jsx

--- class-1/src/Script.jsx
+++ class-1/src/Script.jsx
@@ -2,22 +2,28 @@ import logo from './logo.svg';
 import './Script.css';
 
 function Script() {
+
+  const name = 'Brian'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <p>html</p>
+        
+      <p>{'Brian'}</p>
+
+      <p>{1}</p>
+
+      <p>{[1, 2, 3]}</p>
+
+      <p>[1, 2, 3]</p>
+      
+      <p>{name}</p>
+
+      {/* <p>{true}</p> */}
+
+      {/* <p>{{a: 1, b: 2}}</p> */} 
+      
     </div>
   );
 }
